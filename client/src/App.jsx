35a7c320af3b3ba31524ff1dcc567c67e61ee167
Baran <bar.yildiran@gmail.com>
2024-03-08 17:32:38 -0700

    latest dep

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+
+
+// import Banner from "./components/LandingPage_components/Banner"
+// import Carousel from "./components/LandingPage_components/Carosel"
+// import GroceryNav from "./components/LandingPage_components/GroceryNav"
+import LandingPage from './components/LandingPageComponents/LandingPage';
+
+import IndividualItem from "./components/IndividualItemComponents/IndividualItem"
+
+import Rootlayout from './components/Layout/Rootlayout';
+
+import Deals from './components/DealsPage/Deals';
+
+import Login from './components/LoginSignup/Login';
+
+import ShopMain from './components/Shop/ShopMain';
+
+import Checkout from './components/Checkout/Checkout';
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+
+  const [user,setUser] = useState(null)
+
+  let [cart,setCart] = useState([])
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:5555/check_session')
+    .then(response => response.json())
+    .then(data => {
+      if (data.username){
+        setUser(data)
+      }}
+      )
+  },[])
+
+  console.log(user)
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Rootlayout cart={cart}/>}>
+        <Route index element= { <LandingPage /> } />
+          <Route path="/item" element=  { <IndividualItem /> } />
+          <Route path="/deals" element= { <Deals /> } />
+          <Route path='/login' element= { <Login setUser={setUser}/>} />
+          <Route path='/shop' element=  { <ShopMain cart={cart} setCart={setCart}/>} />
+          <Route path='/checkout' element= {<Checkout cart={cart} setCart={setCart}/>} />
+      </Route>
+    )
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-white text-black font-lato'>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
